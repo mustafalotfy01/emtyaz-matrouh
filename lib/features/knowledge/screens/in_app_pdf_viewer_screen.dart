5816d8212fd45533f5af25670e4c44f505255c38
Mustafa Lotfy <mustafalotfy01@gmail.com>
2026-08-25 16:12:42 +0300

    books update8

--- lib/features/knowledge/screens/in_app_pdf_viewer_screen.dart
+++ lib/features/knowledge/screens/in_app_pdf_viewer_screen.dart
@@ -13,6 +13,7 @@ import '../models/knowledge_article.dart';
 import '../providers/knowledge_provider.dart';
 import '../services/google_drive_document_service.dart';
 import '../services/pdf_cache_service.dart';
+import '../widgets/web_pdf_iframe.dart';
 
 class InAppPdfViewerScreen extends ConsumerStatefulWidget {
   final KnowledgeArticle article;
@@ -73,6 +74,13 @@ class _InAppPdfViewerScreenState extends ConsumerState<InAppPdfViewerScreen> {
       setState(() {
         _isLoading = false;
         _errorMessage = 'معرّف المرجع غير موجود أو الرابط غير صالح.';
+      });
+      return;
+    }
+
+    if (kIsWeb) {
+      setState(() {
+        _isLoading = false;
       });
       return;
     }
@@ -690,6 +698,13 @@ class _InAppPdfViewerScreenState extends ConsumerState<InAppPdfViewerScreen> {
           ),
         ),
       );
+    }
+
+    // Render Embedded PDF Viewer for Flutter Web
+    if (kIsWeb) {
+      final fileId = widget.article.driveFileId ??
+          GoogleDriveDocumentService.extractFileId(widget.article.driveFileUrl ?? '') ?? '';
+      return WebPdfIframe(fileId: fileId, title: widget.article.title);
     }
 
     // Render Native Embedded PDF
--- lib/features/knowledge/widgets/web_pdf_iframe_web.dart
+++ lib/features/knowledge/widgets/web_pdf_iframe_web.dart
@@ -16,18 +16,41 @@ class WebPdfIframe extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewType = 'gdrive-pdf-iframe-$fileId';
 
-    // Register iframe view factory for Google Drive embedded preview
+    // Register iframe view factory for Google Drive embedded preview with popout blocker
     ui_web.platformViewRegistry.registerViewFactory(
       viewType,
       (int viewId) {
+        final container = web.document.createElement('div') as web.HTMLDivElement
+          ..style.position = 'relative'
+          ..style.width = '100%'
+          ..style.height = '100%'
+          ..style.overflow = 'hidden'
+          ..style.backgroundColor = '#000000';
+
         final iframe = web.document.createElement('iframe') as web.HTMLIFrameElement
           ..src = 'https://drive.google.com/file/d/$fileId/preview'
           ..style.border = 'none'
           ..style.width = '100%'
           ..style.height = '100%'
           ..setAttribute('allow', 'autoplay')
-          ..setAttribute('allowfullscreen', 'true');
-        return iframe;
+          ..setAttribute('allowfullscreen', 'true')
+          ..setAttribute('sandbox', 'allow-scripts allow-same-origin allow-forms');
+
+        // Blocker overlay placed over the top-right corner to conceal and block the pop-out icon
+        final blocker = web.document.createElement('div') as web.HTMLDivElement
+          ..style.position = 'absolute'
+          ..style.top = '0'
+          ..style.right = '0'
+          ..style.width = '64px'
+          ..style.height = '56px'
+          ..style.backgroundColor = '#000000'
+          ..style.zIndex = '99999'
+          ..style.pointerEvents = 'auto'
+          ..style.cursor = 'default';
+
+        container.appendChild(iframe);
+        container.appendChild(blocker);
+        return container;
       },
     );
 
