4308a3e9c58c4c74e214cead1fe8625bdbd87868
Mustafa Lotfy <mustafalotfy01@gmail.com>
2026-08-25 16:50:39 +0300

    books update10

--- lib/features/knowledge/widgets/web_pdf_iframe_web.dart
+++ lib/features/knowledge/widgets/web_pdf_iframe_web.dart
@@ -16,40 +16,19 @@ class WebPdfIframe extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewType = 'gdrive-pdf-iframe-$fileId';
 
-    // Register iframe view factory for Google Drive embedded preview with popout blocker
+    // Register iframe view factory for full native Google Drive embedded preview
     ui_web.platformViewRegistry.registerViewFactory(
       viewType,
       (int viewId) {
-        final container = web.document.createElement('div') as web.HTMLDivElement
-          ..style.position = 'relative'
-          ..style.width = '100%'
-          ..style.height = '100%'
-          ..style.overflow = 'hidden'
-          ..style.backgroundColor = '#000000';
-
         final iframe = web.document.createElement('iframe') as web.HTMLIFrameElement
           ..src = 'https://drive.google.com/file/d/$fileId/preview'
           ..style.border = 'none'
           ..style.width = '100%'
           ..style.height = '100%'
-          ..setAttribute('allow', 'autoplay')
+          ..style.touchAction = 'pan-x pan-y pinch-zoom'
+          ..setAttribute('allow', 'autoplay; fullscreen')
           ..setAttribute('allowfullscreen', 'true');
-
-        // Blocker overlay placed over the top-right corner to conceal and block the pop-out icon
-        final blocker = web.document.createElement('div') as web.HTMLDivElement
-          ..style.position = 'absolute'
-          ..style.top = '0'
-          ..style.right = '0'
-          ..style.width = '64px'
-          ..style.height = '56px'
-          ..style.backgroundColor = '#000000'
-          ..style.zIndex = '99999'
-          ..style.pointerEvents = 'auto'
-          ..style.cursor = 'default';
-
-        container.appendChild(iframe);
-        container.appendChild(blocker);
-        return container;
+        return iframe;
       },
     );
 
